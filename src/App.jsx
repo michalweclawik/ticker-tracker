e63822be87593e7d63dd8d8b4868e47fd3365439
michal <michalwagh@gmail.com>
2022-09-05 16:28:09 +0100

    second commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.scss';
+
+function App() {
+  return (
+     <div className = "app" >
+    <header className = "App-header" >
+
+    </header> 
+    </div>
+  );
+}
+
+export default App;
