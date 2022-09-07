974f2b869705532bc55218ee7868a02f272571fe
michal <michalwagh@gmail.com>
2022-09-07 13:24:10 +0100

    fixed bug -callback in setNumber

--- src/counterTracker/CounterTracker.jsx
+++ src/counterTracker/CounterTracker.jsx
@@ -1,17 +1,44 @@
-import React from 'react'
-import "./CounterTracker.scss"
+import React from "react";
+import { useState } from "react";
+import "./CounterTracker.scss";
 
 const CounterTracker = () => {
+  let [number, setNumber] = useState(0);
+
+  const handleClick = (e) => {
+    if (e.target.value === "+") {
+      setNumber((number = number + 1));
+      console.log(number);
+    } else if (number === 0) {
+      return;
+    } else {
+      setNumber((number = number - 1));
+      console.log(number);
+    }
+  };
+
   return (
     <div className="counterTracker">
       <p>Complited ticket</p>
       <div className="counter">
-        <button className="counterButtonMinus">-</button>
-        <div className="counterNumber">15</div>
-        <button className="counterButtonPlus">+</button>
+        <button
+          value="-"
+          onClick={(e) => handleClick(e)}
+          className="counterButtonMinus"
+        >
+          -
+        </button>
+        <div className="counterNumber">{number}</div>
+        <button
+          value="+"
+          onClick={(e) => handleClick(e)}
+          className="counterButtonPlus"
+        >
+          +
+        </button>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default CounterTracker
+export default CounterTracker;
